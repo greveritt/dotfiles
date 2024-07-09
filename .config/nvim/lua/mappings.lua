f7b5cfc7c45836d2955a3221ca3e6af06d04c03b
Gregory Everitt <gregory.everitt@docgo.com>
2024-07-09 12:02:36 -0400

    Ports remaining keymaps to Lua

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -16,3 +16,22 @@ map("n", "<Leader>r", ':TestVisit<CR>')
 
 -- Telescope
 map("n", "<Leader>fs", "<cmd>Telescope lsp_workplace_symbols<CR>", { desc = "Telescope lsp workplace symbols" })
+
+-- Gitsigns
+map("n", "<Leader>gn", "<cmd>Gitsigns next_hunk<CR>", { desc = "Move to next Git hunk" })
+map("n", "<Leader>gp", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Move to previous Git hunk" })
+
+-- Fugitive
+map("n", "<Leader>gs", "<cmd>Git<CR>", { desc = "Git status" })
+map("n", "<Leader>gc", "<cmd>Git commit<CR>", { desc = "Git commit" })
+map("n", "<Leader>gb", "<cmd>Git blame<CR>", { desc = "Git blame" })
+
+
+-- Remaps
+local unmap = vim.keymap.del
+
+-- Splits
+unmap("n", "<Leader>v", { desc = "terminal new vertical window" })
+unmap("n", "<Leader>h", { desc = "terminal new horizontal term" })
+map("n", "<Leader>v", "<cmd>vsplit<CR>", { desc = "splits current window vertically" })
+map("n", "<Leader>h", "<cmd>split<CR>", { desc = "splits current window horizontally" })
--- .config/nvim/todo.vim
+++ .config/nvim/todo.vim
@@ -18,16 +18,6 @@ else
 endif
 let test#ruby#use_spring_binstub = 1
 
-" Git
-noremap <Leader>gs :Git<CR>
-noremap <Leader>gc :Git commit<CR>
-noremap <Leader>gb :Git blame<CR>
-noremap <Leader>gn :Gitsigns next_hunk<CR>
-noremap <Leader>gp :Gitsigns prev_hunk<CR>
-
-" Close buffer
-"noremap <leader>c :bd<CR>
-
 "nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 " set foldmethod=expr
 " set foldexpr=nvim_treesitter#foldexpr()
