09491b77e9e477532c7e257f2bd92150f72088df
Gregory Everitt <gregory.everitt@docgo.com>
2024-08-29 09:59:38 -0400

    Fold with treesitter

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,5 +2,8 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+o.foldmethod = 'expr'
+o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+o.foldenable = false -- Open buffers unfolded
